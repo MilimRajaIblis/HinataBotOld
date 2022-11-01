661dec0e7207483d6108c23a286e4398560d9761
Wudysoft <116274228+wudysoft@users.noreply.github.com>
2022-11-01 11:48:44 +0800

    New
    New

--- main.js
+++ main.js
@@ -26,7 +26,8 @@ import syntaxerror from 'syntax-error'
 import { tmpdir } from 'os'
 import { format } from 'util'
 import { makeWASocket, protoType, serialize } from './lib/simple.js'
-import { Low, JSONFile } from 'lowdb'
+import { Low } from 'lowdb'
+import { JSONFile } from 'lowdb/node'
 import {
   mongoDB,
   mongoDBV2

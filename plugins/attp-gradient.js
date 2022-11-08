a9587e41abc1247f8926b7368fd32040256c38f7
Wudysoft <116274228+wudysoft@users.noreply.github.com>
2022-11-08 18:16:16 +0800

    New
    New

--- plugins/attp-gradient.js
+++ plugins/attp-gradient.js
@@ -22,26 +22,26 @@ let urls = ['https://violetics.pw/api/canvas/quotes?apikey=beta&text=' + teks + 
 'https://violetics.pw/api/canvas/attp-gradient2?apikey=beta&text=' + teks,
 'https://violetics.pw/api/canvas/attp-gradient?apikey=beta&text=' + teks,
 'https://violetics.pw/api/canvas/ttp-gradient?apikey=beta&text=' + teks]
-
-            let lisn = ["Quotes",
-            "Attp 2",
-            "Attp",
-            "Harta Tahta",
-            "TTP 2",
-            "TTP 3",
-            "TTP 4",
-            "TTP 5",
-            "TTP 6",
-            "TTP",
-            "TTP 7",
-            "TTP 8",
-            "Attp Gradient 2",
-            "Attp Gradient",
-            "TTP Gradient"
+            let lisn = ["Quotes: " + urls[0].substring(8, urls[0].indexOf('/api')),
+            "Attp 2: " + urls[1].substring(8, urls[1].indexOf('/api')),
+            "Attp: " + urls[2].substring(8, urls[2].indexOf('/api')),
+            "Harta Tahta: " + urls[3].substring(8, urls[3].indexOf('/api')),
+            "TTP 2: " + urls[4].substring(8, urls[4].indexOf('/api')),
+            "TTP 3: " + urls[5].substring(8, urls[5].indexOf('/api')),
+            "TTP 4: " + urls[6].substring(8, urls[6].indexOf('/api')),
+            "TTP 5: " + urls[7].substring(8, urls[7].indexOf('/api')),
+            "TTP 6: " + urls[8].substring(8, urls[8].indexOf('/api')),
+            "TTP: " + urls[9].substring(8, urls[9].indexOf('/api')),
+            "TTP 7: " + urls[10].substring(8, urls[10].indexOf('/api')),
+            "TTP 8: " + urls[11].substring(8, urls[11].indexOf('/api')),
+            "Attp Gradient 2: " + urls[12].substring(8, urls[12].indexOf('/api')),
+            "Attp Gradient: " + urls[13].substring(8, urls[13].indexOf('/api')),
+            "TTP Gradient: " + urls[14]].substring(8, urls[14].indexOf('/api'))
             ]
+            
             let row = Object.keys(urls, lisn).map((v, index) => ({
 		title: htjava + ' ' + lisn[v] + ' Sticker',
-		description: 'By ' + wm,
+		description: 'By: ' + wm + '\n Link: ' + urls[v].substring(8, urls[v].indexOf('/api')),
 		rowId: usedPrefix + 'get ' + urls[v]
 	}))
 	let button = {

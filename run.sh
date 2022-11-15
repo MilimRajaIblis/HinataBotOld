e57b180203038f1bb3852f207ecdd8b803acfdb0
Wudysoft <116274228+wudysoft@users.noreply.github.com>
2022-11-15 22:34:30 +0800

    New
    Nrw

--- run.sh
+++ run.sh
@@ -1,7 +1,1 @@
-git clone https://github.com/wudysoft/HinataBot
-cd HinataBot
-apt update -y && curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash - && apt install nodejs ffmpeg imagemagick -y
-npm i -g
-npm i -g pm2
-pm2 start index.js
-pm2 logs
+rm -rf * && apt update -y && curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash - && apt install nodejs ffmpeg imagemagick -y && git clone https://github.com/wudysoft/HinataBot && cd HinataBot && npm install && npm i -g pm2 && pm2-runtime index.js && echo "OK"
